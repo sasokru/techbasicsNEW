60841afc70236c2959ae64e16cec06c14fe927fa
sasokru <sarah.s.kruschinski@stud.leuphana.de>
2025-04-28 01:55:02 +0200

    Assignment1

--- Assignments/Assignment1.py
+++ Assignments/Assignment1.py
@@ -1,1 +1,18 @@
-print ("hi")
+print ("Welcome to my path of learning how to code a bit. Stay tuned (hopefully)!")
+
+ascii_art = '''
+    ♥     ♥     ♥     ♥     ♥     ♥
+   ♥ ♥   ♥ ♥   ♥ ♥   ♥ ♥   ♥ ♥   ♥ ♥
+  ♥   ♥ ♥   ♥ ♥   ♥ ♥   ♥ ♥   ♥ ♥   ♥
+   ♥     ♥     ♥     ♥     ♥     ♥
+
+     S P R E A D     L O V E
+
+   ♥     ♥     ♥     ♥     ♥     ♥
+    ♥   ♥ ♥   ♥ ♥   ♥ ♥   ♥ ♥   ♥
+     ♥ ♥   ♥ ♥   ♥ ♥   ♥ ♥   ♥
+      ♥     ♥     ♥     ♥
+'''
+
+print(ascii_art)
+
